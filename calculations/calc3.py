2fd945dea0c381a18b0e3bb99fc5741cb5a317e7
Frank Hofmann <frank.hofmann@efho.de>
2014-11-18 14:55:07 +0100

    calculations - part two, a list of integers using the built-in sum function

---
+++ calculations/calc3.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------
+# calculate the total for a list of items
+# 
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define a list of single values
+valueList = [100, 13, 35, 8, -7, 1688]
+
+# number of items
+numberOfItems = len(valueList)
+
+# define total value
+total = 0
+
+# calculate the total sum using the built-in function sum
+total = sum(valueList)
+
+# output the total sum
+# ... using C-like syntax
+print ("Total for %i items: %i" % (numberOfItems, total))
