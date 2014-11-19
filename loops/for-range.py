8d395d8d2bef53fd625f31a6bbb7c6e8dcefeef3
Frank Hofmann <frank.hofmann@efho.de>
2014-11-19 11:09:57 +0100

    loops - basic for loop with range definition in a list

---
+++ loops/for-range.py
@@ -0,0 +1,29 @@
+# -----------------------------------------------------------
+# demonstrates the usage of a for loop to interate through a list
+# using a range
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define list
+shoppingCart = ["banana", "apple", "grapefruit", "carrot", "potato"]
+
+# define list boundaries
+lowerBound = 2
+upperBound = 4
+
+print ("lower bound:", lowerBound)
+print ("upper bound:", upperBound)
+print ()
+
+# get the number of list items
+listRange = range(lowerBound, upperBound + 1)
+listItems = len(listRange)
+for itemIndex in listRange:
+	print (itemIndex, shoppingCart[itemIndex])
+
+print ("----------------")
+print ("total:", listItems, "items")
+
