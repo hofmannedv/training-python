8a7071693bddb6f7fe2098c6c0d476c790ee8365
Frank Hofmann <frank.hofmann@efho.de>
2014-11-27 23:00:01 +0100

    assignments, multiple assignments

--- assignments/assignments.py
+++ assignments/assignments.py
@@ -14,3 +14,16 @@ length = 2.5					# float value assignment
 print ("colour:", colour)
 print ("size  :", size)
 print ("length:", length)
+
+# multiple assignments
+a = b = c = 1					# set a, b, and c to 1
+d,e = 2, 3						# set d to 2, and e to 3
+print (("a,b,c : %i,%i,%i") % (a,b,c))
+print (("d,e   : %i,%i") % (d,e))
+
+# assignments with calculations
+total = a + b - c + d -e		# simple addition
+output = colour * 4				# concatenate a string four times
+print (("total : %i") % (total))
+print (("colour: %s") % (colour))
+print (("output: %s") % (output))
