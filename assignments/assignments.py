7997113101fec63cbc18e594cd8eaaf947d9805e
Frank Hofmann <frank.hofmann@efho.de>
2014-11-27 22:47:36 +0100

    assignments, single assignments

---
+++ assignments/assignments.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------
+# demonstrates different kind of assignments
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# single assignments
+colour = "blue"					# string assignment
+size = 17						# integer assignment
+length = 2.5					# float value assignment
+
+print ("colour:", colour)
+print ("size  :", size)
+print ("length:", length)
