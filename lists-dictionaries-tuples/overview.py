5e5023a360510056cb23aa497cf4641ef707da14
Frank Hofmann <frank.hofmann@efho.de>
2014-11-29 15:51:55 +0100

    lists, dictionaries and tuples

---
+++ lists-dictionaries-tuples/overview.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------
+# demonstrates how to create lists, dictionaries, and tuples
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# list of numbers
+numbersList = [394, 6, 15, 2, 88, 153]
+# a list with mixed values
+mixedList = [1184, "orange", 3.56, True]
+print (numbersList)
+print (mixedList)
+print (numbersList + mixedList)
+
+# a tuple of numbers (a fixed list that cannot be changed)
+numbersTuple = (23, 17, -6, 516)
+smallTuple = (45, "Henry")
+print (numbersTuple)
+print (smallTuple)
+print (numbersTuple + smallTuple)
+
