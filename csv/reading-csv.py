16e210e2952476b68369433b64d4d7d1df857427
Frank Hofmann <frank.hofmann@efho.de>
2014-12-09 17:23:40 +0100

    how to read data from a csv file, and output desired columns

---
+++ csv/reading-csv.py
@@ -0,0 +1,46 @@
+# -----------------------------------------------------------
+# reads the given data file, and imports it as csv file
+#o
+# (C) 2014 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# call the program this way:
+# python reading-csv.py database.txt
+
+# import required python standard modules
+import sys,csv
+
+# count number of program parameters
+numPara = len(sys.argv)
+if numPara < 2:
+	print ("invalid number of parameters: 1 required.")
+	print ("call: python %s database.txt" % sys.argv[0])
+	print ("Exiting.")
+	sys.exit(2)
+
+# read name of the datafile
+datafileName = sys.argv[1]
+print ("reading database from", datafileName, "...")
+
+lines = 0
+with open(datafileName) as csvfile:
+	reader = csv.DictReader(csvfile)
+
+	# read the data from csv file, and output the data fields
+	# the  first line contains the names of the columns that are used as keys
+
+	for row in reader:
+		# read columns
+		nameEntry, addressEntry, cityEntry, zipEntry, countryEntry = row["Name"], row["Address"], row["City"], row["Zip"], row["Country"]
+
+		print (("Name and place: %s,%s") % (nameEntry, cityEntry))
+
+		# increase the line counter
+		lines += 1
+
+# output the number of lines read from database
+print ("... read", lines, "lines")
+
+
