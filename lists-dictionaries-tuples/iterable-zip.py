2f9d5145fdfef225b222a0baa7cfe8d118a044d6
Frank Hofmann <frank.hofmann@efho.de>
2015-02-15 12:58:29 +0100

    lists and dictionaries, zip function

---
+++ lists-dictionaries-tuples/iterable-zip.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------
+# demonstrates how to use the zip function
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define variables zipCode, place
+zipCode = ["10247", "01015", "80123", "14467"]
+place = ["Berlin", "Dresden", "München", "Potsdam"]
+
+# combine both zipCode and place
+address = zip(zipCode, place)
+
+# output address
+for item in address:
+	print (item)
