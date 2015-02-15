9c3b673482f37de737c82df1472a5bb63bb1e65b
Frank Hofmann <frank.hofmann@efho.de>
2015-02-15 22:31:54 +0100

    lists, dictionaries and tuples: sorting and reversed order

---
+++ lists-dictionaries-tuples/sort-reverse.py
@@ -0,0 +1,26 @@
+# -----------------------------------------------------------
+# demonstrates how to sort lists
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define basic list
+numbers = [56, 22, 73, 105]
+
+# sorting numbers ascending
+sortedNumbers = sorted(numbers)
+
+# reversing the list
+reversedNumbers = reversed(numbers)
+
+# output
+print("original list:", numbers)
+print("sorted list:", sortedNumbers)
+
+# reversed returns an iterator object, so we have to use a loop
+print("reversed list:")
+for i in reversedNumbers:
+	print(i, end=" ")
+print()
