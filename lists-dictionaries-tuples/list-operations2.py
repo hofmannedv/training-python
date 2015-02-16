af45cfce3d97a908f5c0ae2e77253eec56ca463b
Frank Hofmann <frank.hofmann@efho.de>
2015-02-16 16:07:34 +0100

    lists and dictionaries, various basic list operations, part 2

---
+++ lists-dictionaries-tuples/list-operations2.py
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------
+# demonstrates various basic list operations
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define two lists
+cities1 = ["Paris", "Bordeaux", "Metz"]
+cities2 = ["Bern", "Locarno", "Lausanne"]
+
+print(cities1)
+print(cities2)
+
+# adding another list element
+cities1.append("Lyon")
+print("modified:", cities1)
