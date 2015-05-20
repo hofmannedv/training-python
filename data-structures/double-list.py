496c78a60c1604d276b15b8aaf6b651f9acdf2cb
Frank Hofmann <frank.hofmann@efho.de>
2015-05-20 23:04:50 +0200

    data structures, double-linked list class with references

---
+++ data-structures/double-list.py
@@ -0,0 +1,45 @@
+# -----------------------------------------------------------
+# demonstrates how to create and use a simple-linked list
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+class ListNode:
+	def __init__(self, data):
+		"constructor class to initiate this object"
+
+		# store data
+		self.data = data
+		
+		# store reference (next item)
+		self.next = None
+		return
+
+# main program
+
+# create three single nodes
+node1 = ListNode(15)
+node2 = ListNode(8.2)
+node3 = ListNode("Berlin")
+
+# link the objects
+node1.next = node2
+node2.next = node3
+
+# output the full node list
+# define start node
+startNode = node1
+
+# define currentNode
+currentNode = startNode
+
+# loop through the list of nodes up to the end
+while currentNode is not None:
+	# retrieve and output node data
+	data = currentNode.data
+	print ("node:", data)
+	
+	# jump to the linked node
+	currentNode = currentNode.next
