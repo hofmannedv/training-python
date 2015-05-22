947a78b263a3c8d90a15dfb6f905b9539d2a17e9
Frank Hofmann <frank.hofmann@efho.de>
2015-05-22 16:01:44 +0200

    added stack class

---
+++ data-structures/stack.py
@@ -0,0 +1,64 @@
+# -----------------------------------------------------------
+# create a stack class
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+#
+
+# stack class
+
+class Stack:
+	def __init__(self):
+		"initiate this object"
+		self.items = []
+		return
+
+	def push(self, item):
+		"store an item on the stack"
+		self.items.append(item)
+		return
+
+	def pop(self):
+		"retrieve an item from the stack"
+		if not self.isEmpty():
+			return self.items.pop()
+		else:
+			return False
+
+	def isEmpty(self):
+		"check for an empty stack"
+		return (self.items == [])
+
+	def size(self):
+		"count the elements on the stack"
+		return len(self.items)
+
+# main program
+stack = Stack()
+
+# define data
+data = [15, 7, [4, 23], 39]
+
+# go through it step by step
+for item in data:
+	if isinstance(item, int):
+		# we have an integer
+		stack.push(item)
+	elif isinstance(item, list):
+		# we have a list of integers
+		for listItem in item:
+			stack.push(listItem)
+
+# count total
+total = 0
+
+# take the items from the stack
+while not stack.isEmpty():
+	value = stack.pop()
+	print ("added %i to %i" % (value, total))
+	total += value
+
+# output total
+print ("total:", total)
