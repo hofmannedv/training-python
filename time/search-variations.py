43446810dedbf7a54b99e48c82626010b427719e
Frank Hofmann <frank.hofmann@efho.de>
2015-06-28 14:48:53 +0200

    time measurements: searching numbers in a list

--- time/search-variations.py
+++ time/search-variations.py
@@ -6,6 +6,37 @@
 # email frank.hofmann@efho.de
 # -----------------------------------------------------------
 
+# define python standard module
+import time
+
+def incrementalSearch(sortedNumberList, number):
+
+	# verify the list elements one by one
+
+	# define minimal element	
+	minElement = 0
+
+	# define maximum element
+	maxElement = len(sortedNumberList)
+
+	# start with minimal element
+	currentElement = minElement
+
+	while currentElement < maxElement:
+		# did we find the element in the list?
+		if sortedNumberList[currentElement] == number:
+			# yes, return its position in the list
+			print ("got %i at position %i" % (number, currentElement))
+			return currentElement
+		else:
+			# no, continue searching
+			print ("comparison of %i at position %i failed" % (number, currentElement))
+			currentElement += 1
+
+	# if not successfull, return -1
+	print ("element %i not found" % number)
+	return -1
+
 def binarySearch(sortedNumberList, number):
 
 	# use a binary search to look for the given number in that sorted
@@ -48,6 +79,15 @@ number = 23
 # define the list of numbers
 sortedNumberList = (1, 5, 12, 17, 18, 19, 26, 32, 33, 36, 37, 47, 89)
 
+# evaluate incremental search
+startTime1 = time.time()
+listPosition = incrementalSearch(sortedNumberList, number)
+endTime1 = time.time()
+
+# calculate and output interval time
+seconds = endTime1 - startTime1
+print ("incremental search took %.8f seconds" % seconds)
+
 # evaluate binary search
 startTime1 = time.time()
 listPosition = binarySearch(sortedNumberList, number)
@@ -55,6 +95,6 @@ endTime1 = time.time()
 
 # calculate and output interval time
 seconds = endTime1 - startTime1
-print ("binary search took %.2f seconds" % seconds)
+print ("binary search took %.8f seconds" % seconds)
 
 
