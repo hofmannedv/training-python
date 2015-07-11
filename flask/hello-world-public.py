675ef58ad4631ead6e282c83eda2e16a61ddc61f
Frank Hofmann <frank.hofmann@efho.de>
2015-07-11 15:54:54 +0200

    flask: basic flask application with debug mode, README file

---
+++ flask/hello-world-public.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------
+# basic flask application with public access, and in debug mode
+#o
+# (C) 2015 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# import flask module
+from flask import Flask
+
+# define flask application
+application = Flask(__name__)
+
+# define application path
+@application.route('/')
+def helloWorld():
+	# output welcome message
+	return ("Hello, World! Nice to see you!")
+
+# main application start
+# - available/access from everywhere (set host to "0.0.0.0")
+# - debug mode enabled (set debug to True)
+# - reloading the server automatically if python code changes
+if __name__ == "__main__":
+	application.run(host="0.0.0.0", debug=True)
+
+	# alternatively, enable the application debug flag like this:
+	# application.debug = True
+
