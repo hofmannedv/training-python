ec8eef117ebac7354b829699980c61d1e6aaed92
Frank Hofmann <frank.hofmann@efho.de>
2015-07-16 13:49:51 +0200

    python xml: README and podcast SAX demo

--- xml/podcast.py
+++ xml/podcast.py
@@ -16,22 +16,25 @@ class podcastHandler(handler.ContentHandler):
 
 	def startElement(self, name, attrs):
 
+		# define pattern element
 		pattern = "enclosure"
 		prog = re.compile(pattern)
 
+		# look for the given element
 		if re.match(prog, name):
+			# output its name, uri, and content length
 			print (name)
 			print (attrs.get("url", ""))
 			print (attrs.get("length", ""))
 
 		return
 
-# define podcat parser
+# define podcast parser
 parser = make_parser()
 
 # attach podcast handler
 p = podcastHandler()
 parser.setContentHandler(p)
 
-# extract data
+# extract data from this xml file
 parser.parse("hackerfunkfeed.xml")
