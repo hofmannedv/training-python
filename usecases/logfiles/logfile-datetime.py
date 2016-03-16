9378aaadf3cc40239c5fd316e92f3d76eea2b782
Frank Hofmann <frank.hofmann@efho.de>
2016-03-16 17:50:57 +0100

    use cases: read logfile by timestamp range (with regex)

--- usecases/logfiles/logfile-datetime.py
+++ usecases/logfiles/logfile-datetime.py
@@ -56,20 +56,24 @@ def findEntryByTimestampRange(fileContent, fromTimestamp, toTimestamp):
 	# define empty entry list
 	entries = []
 
-	# define timestamp patterns
-	# format: year-month-day hour:minute:second
-	fromTimestampPattern = fromTimestamp.strftime("%Y-%m-%d %H:%M:%S")
-	toTimestampPattern = toTimestamp.strftime("%Y-%m-%d %H:%M:%S")
-
+	# define the timestamp search pattern for our logfile
+	# year-month-day hour:minute:second
 	pattern = re.compile('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}')
 
 	# go through the file content line by line
 	for line in fileContent:
 		# if we find the pattern in the current line ...
-		if re.findall(pattern, line):
-			print (re.findall(pattern, line))
-			# ... add the line to the list of entries
-			entries.append(line)
+		matches = re.findall(pattern, line)
+		if matches:
+			# parse the first match to create a datetime object
+			currentTimestamp = datetime.strptime(matches[0], "%Y-%m-%d %H:%M:%S")
+
+			# check if the current timestamp is in the given range
+			if currentTimestamp >= fromTimestamp:
+				if currentTimestamp <= toTimestamp:
+					# print (re.findall(pattern, line))
+					# ... add the line to the list of entries
+					entries.append(line)
 
 	return entries
 
@@ -109,14 +113,17 @@ for singleEntry in entries:
 
 # find entries within a timestamp range
 # - date
-fromDate = date(2016, 1, 10)
-toDate = date(2016, 1, 10)
+fromDate = date(2016, 1, 12)
+toDate = date(2016, 1, 12)
 # - time
-fromTime = time(8, 0, 0)
-toTime = time(15, 0, 0)
+fromTime = time(14, 45, 0)
+toTime = time(14, 55, 59)
 # - timestamp
 fromTimestamp = datetime.combine(fromDate, fromTime)
 toTimestamp = datetime.combine(toDate, toTime)
 
 print (("entries with timestamp from %s to %s:") % (fromTimestamp.ctime(), toTimestamp.ctime()))
 entries = findEntryByTimestampRange(fileContent, fromTimestamp, toTimestamp)
+for singleEntry in entries:
+	print (singleEntry)
+
