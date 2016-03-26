406b3760b23a58095a3360daf74913716b79ef16
Frank Hofmann <frank.hofmann@efho.de>
2016-03-26 13:22:03 +0100

    client-server: fixing tcp client, basic setup

--- server-client/client-tcp.py
+++ server-client/client-tcp.py
@@ -12,6 +12,29 @@
 # -----------------------------------------------------------
 
 import socket
+import sys
+import re
+
+def receiveAllData(socketId, length):
+	# receive all data for a request
+	
+	# received data is empty (default value)
+	data = ''
+
+	# read data as long as there is data to read
+	while len(data) < length:
+		# try to read as many data as possible
+		more = socketId.recv(length - len(data))
+		if not more:
+			raise EOFError('[client] socket closed %d bytes into a %d-byte message' % (len(data), length))
+
+		# combine the data we retrieved so far
+		data += more
+	
+	# return the data we retrieved
+	return data
+
+# ---- main program ----
 
 # use localhost
 host = ""
@@ -22,25 +45,20 @@ port = 12345
 # define packet data size
 dataSize = 1024
 
-# init socket
+# init stream socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-requests = ["Hello", "3 + 4", "exit"]
 
 # connect to server
 s.connect((host,port))
 print (("[client] connecting to server at %s:%i") % (host, port))
+print ("[client] assigned socket name:", s.getsockname())
 
-for data in requests:
-	# send data
-	print ("[client] sending data: %s" % data)
-	s.send(data)
+# send data to server
+s.sendall('Hi there, server')
 
-	# receive answer
-	data = s.recv(dataSize)
-
-	# output received data
-	print ("[client] Received answer:", data)
+# retrieve reply from server
+reply = receiveAllData(s, 16)
+print ('[client] the server replied: %s' % repr(reply))
 
 # close connection
 s.close()
