3d21d4312aa94aa9b492d58728bb3b486e5c1f66
Frank Hofmann <frank.hofmann@efho.de>
2016-03-30 15:12:13 +0200

    network: web server test (extended)

--- network/gethttpservice.py
+++ network/gethttpservice.py
@@ -23,3 +23,32 @@ if len(sys.argv) != 2:
 hostname = sys.argv[1]
 print ("checking for domain %s ..." % hostname)
 
+try:
+	# request data for an www service listening
+	infolist = socket.getaddrinfo(
+	hostname, 'www', 0, socket.SOCK_STREAM, 0,
+	socket.AI_ADDRCONFIG | socket.AI_V4MAPPED | socket.AI_CANONNAME
+	)
+except socket.gaierror, errorState:
+	# no address associated with this hostname
+	print ('Name service failure: %s' % (errorState.args[1]))
+	sys.exit(1)
+
+# output the retrieved information
+# try the first one as recommended per standard
+info = infolist[0]
+
+# retrieve the socket arguments
+socketArguments = info[0:3]
+
+# retrieve the ip address
+address = info[4]
+
+# try to connect to the retrieved ip address
+s = socket.socket(*socketArguments)
+try:
+	s.connect(address)
+except socket.error, errorState:
+	print ('Network failure: %s' % errorState.args[1])
+else:
+	print ('Success. The host %s is listening on port 80' % info[3])
