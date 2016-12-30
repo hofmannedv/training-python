0327ce5180c3fe00c7dd1910e2d9995bb440a4af
Frank Hofmann <frank.hofmann@efho.de>
2016-12-30 18:58:50 +0100

    calculations: calculate the minimum and maximum of an array element-wise using NumPy methods

---
+++ calculations/min-max-array-numpy.py
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------
+# calculate the minimum, and the maximum value element-wise for 
+# two arrays of integers using the NumPy module
+#o
+# (C) 2016 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# requirements:
+# * Python 3.x
+# * NumPy for Python 3 (http://www.numpy.org/)
+
+# import external NumPy module
+import numpy as np
+
+# define a list of integer values
+valueList1 = np.array([1, 6, 3, 4, 5])
+valueList2 = np.array([2, 4, 3, 1, 9])
+
+print ("data 1: ", valueList1)
+print ("data 2: ", valueList2)
+print (" ")
+
+# calculate maximum and minimum
+print ("calculate the maximum/minimum between data 1 and 2:")
+print ("maximum:", np.maximum(valueList1, valueList2))
+print ("minimum:", np.minimum(valueList1, valueList2))
+print (" ")
+
+# element-wise comparison
+print ("compare data 1 and 2:")
+print ("greater:      ", np.greater(valueList1, valueList2))
+print ("greater-equal:", np.greater_equal(valueList1, valueList2))
+print ("equal:        ", np.equal(valueList1, valueList2))
+print ("not-equal:    ", np.not_equal(valueList1, valueList2))
+print ("less:         ", np.less(valueList1, valueList2))
+print ("less-equal:   ", np.less_equal(valueList1, valueList2))
