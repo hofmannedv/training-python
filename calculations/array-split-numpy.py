4a132d43292dd6c4914424ba7ed1806f922c4fc0
Frank Hofmann <frank.hofmann@efho.de>
2016-12-31 21:36:07 +0100

    calculations: show how to split arrays using NumPy methods

---
+++ calculations/array-split-numpy.py
@@ -0,0 +1,39 @@
+# -----------------------------------------------------------
+# demonstrates how to split an array using the NumPy module
+# 
+# (C) 2016 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# requirements:
+# * Python 3.x
+# * NumPy for Python 3 (http://www.numpy.org/)
+
+# import external NumPy module
+import numpy as np
+
+# define an array 2 x 4
+# [ 1 2 3 4 ]
+# [ 5 6 7 8 ]
+data1 = np.array([[1, 2, 3, 4],[5, 6, 7, 8]])
+
+print ("data:")
+print (data1)
+print (" ")
+
+# split the array vertical
+print ("split the array vertical into two pieces:")
+print (np.vsplit(data1,2))
+print (" ")
+
+# split the array horizontal
+print ("split the array horizontal into two pieces:")
+print (np.hsplit(data1,2))
+print (" ")
+
+# split the array horizontal after the first and third row
+print ("split the array horizontal into three pieces:")
+print (np.hsplit(data1,(1,3)))
+print (" ")
+
