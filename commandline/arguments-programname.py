6b1ea6d21ecf9445b545b583622dcea3e4a06371
Frank Hofmann <frank.hofmann@efho.de>
2017-09-25 15:42:29 +0200

    determine the program/script name using the sys module

---
+++ commandline/arguments-programname.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------
+# demonstrates how to work with commandline arguments
+# determine the program/script name
+#o
+# (C) 2017 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# include standard modules
+import sys
+
+# evaluate the first argument, only
+program = sys.argv[0]
+print (("we are called as %s") % (program))
+
