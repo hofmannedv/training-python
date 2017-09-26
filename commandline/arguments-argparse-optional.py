53e98900bbbb5dbb3d01dac1929503e178ae536d
Frank Hofmann <frank.hofmann@efho.de>
2017-09-26 16:31:23 +0200

    usage of the argparse module with optional argument

---
+++ commandline/arguments-argparse-optional.py
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------
+# demonstrates how to work with commandline arguments
+# using the argparse module (available from Python 3.2)
+# with an optional argument
+#o
+# (C) 2017 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# include standard modules
+import argparse
+
+# initiate the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", help="show program version", action="store_true")
+parser.add_argument("-V", help="show program version", action="store_true")
+parser.parse_args()
+
+# read arguments from the command line
+args = parser.parse_args()
+
+# check for --version
+if args.version or args.V:
+    print("this is myprogram version 0.1")
