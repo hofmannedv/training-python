dbb4737313a354352be7bf4ebb5450fe4ee70261
Frank Hofmann <frank.hofmann@efho.de>
2017-09-26 14:44:17 +0200

    basic usage of the argparse module

---
+++ commandline/arguments-argparse-basic.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------
+# demonstrates how to work with commandline arguments
+# using the argparse module (available from Python 3.2)
+#o
+# (C) 2017 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# include standard modules
+import argparse
+
+# initiate the parser
+parser = argparse.ArgumentParser()
+parser.parse_args()
