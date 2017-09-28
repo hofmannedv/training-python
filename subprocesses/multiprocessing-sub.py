54c3e1c1f8fcd0853936e7d7a85c4f13b82bccad
Frank Hofmann <frank.hofmann@efho.de>
2017-09-28 15:00:37 +0200

    running two functions (subs) in parallel using the multiprocessing module

---
+++ subprocesses/multiprocessing-sub.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------
+# demonstrates how to run two functions in parallel using the
+# multiprocessing library
+#
+# idea taken from this post:
+# https://stackoverflow.com/questions/7207309/python-how-can-i-run-python-functions-in-parallel
+#o
+# (C) 2017 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# include standard modules
+from multiprocessing import Process
+import os
+
+def func1(cycles):
+  print ('func1: starting with %i cycles' % cycles)
+  for i in xrange(cycles): pass
+  print 'func1: finishing'
+
+def func2(cycles):
+  print ('func2: starting with %i cycles' % cycles)
+  for i in xrange(cycles): pass
+  print 'func2: finishing'
+
+if __name__ == '__main__':
+  # getting the process ids
+  print('parent process: %i' % os.getppid())
+  print('process id: %i' % os.getpid())
+
+  # initialising two processes, and starting p1, and p2
+  p1 = Process(target=func1, args=(500000,))
+  p1.start()
+  p2 = Process(target=func2, args=(200000,))
+  p2.start()
+
+  # wait for terminating p1 and p2
+  p1.join()
+  p2.join()
+
