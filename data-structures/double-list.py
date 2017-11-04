5553dee73f7498f647f388c88d814f0388ec1f33
Frank Hofmann <frank.hofmann@efho.de>
2017-11-04 10:47:37 +0100

    data structures: double-linked list, added further methods to add and remove nodes

--- data-structures/double-list.py
+++ data-structures/double-list.py
@@ -43,42 +43,126 @@ class ListNode:
 		else:
 		    return False
 
+class DoubleLinkedList:
+	def __init__(self):
+		"constructor to initiate this object"
+
+		self.head = None
+		self.tail = None
+		return
+
+	def listLength(self):
+		"returns the number of list items"
+		
+		count = 0
+		currentNode = self.head
+
+		while currentNode is not None:
+			# increase counter by one
+			count = count + 1
+			
+			# jump to the linked node
+			currentNode = currentNode.getNext()
+		
+		return count
+
+	def outputList(self):
+		"outputs the list (the value of the node, actually)"
+		currentNode = self.head
+
+		while currentNode is not None:
+			print (currentNode.getData())
+
+			# jump to the linked node
+			currentNode = currentNode.getNext()
+		
+		return
+
+	def addListitem(self, item):
+		"add an item at the end of the list"
+
+		if isinstance(item, ListNode):
+			if self.head is None:
+				self.head = item
+				item.previous = None
+				item.next = None
+				self.tail = item
+			else:
+				self.tail.next = item
+				item.previous = self.tail
+				self.tail = item
+		
+		return
+
+	def unorderedSearch (self, value):
+		"search the linked list for the node that has this value"
+
+		# define currentNode
+		currentNode = self.head
+
+		# define position
+		nodeId = 1
+
+		# define list of results
+		results = []
+
+		while currentNode is not None:
+			if currentNode.hasValue(value):
+				results.append(nodeId)
+			
+			# jump to the linked node
+			currentNode = currentNode.getNext()
+			nodeId = nodeId + 1
+		
+		return results
+
+	def removeListitemById(self, itemId):
+		"remove the list item with the item id"
+		
+		currentId = 1
+		currentNode = self.head
+
+		while currentNode is not None:
+			previousNode = currentNode.getPrevious()
+			nextNode = currentNode.getNext()
+
+			if currentId == itemId:
+				# if this is the first node (head)
+				if previousNode is not None:
+					previousNode.next = nextNode
+					if nextNode is not None:
+						nextNode.previous = previousNode
+				else:
+					self.head = nextNode
+					if nextNode is not None:
+						nextNode.previous = None
+				# we don't have to look any further
+				return
+ 
+			# needed for the next iteration
+			currentNode = nextNode
+			currentId = currentId + 1
+				
+		return
+
 # main program
 
 # create three single nodes
 node1 = ListNode(15)
 node2 = ListNode(8.2)
 node3 = ListNode("Berlin")
+node4 = ListNode(15)
 
-# link the objects
-node1.next = node2
-node2.previous = node1
-node2.next = node3
-node3.previous = node2
+track = DoubleLinkedList()
+print ("track length: %i" % track.listLength())
 
-# output the full node list
-# define start node
-startNode = node1
+for currentNode in [node1, node2, node3, node4]:
+	track.addListitem(currentNode)
+	print ("track length: %i" % track.listLength())
+	track.outputList()
 
-# define currentNode
-currentNode = startNode
+results = track.unorderedSearch(15)
+print (results)
 
-# loop through the list of nodes up to the end (forward)
-while currentNode is not None:
-	# retrieve and output node data
-	data = currentNode.data
-	print ("node:", data)
-	
-	# jump to the linked node
-	currentNode = currentNode.next
-
-# loop though the list of nodes backwards
-currentNode = node3
-while currentNode is not None:
-	# retrieve and output node data
-	data = currentNode.data
-	print ("node:", data)
-	
-	# jump to the previous node
-	currentNode = currentNode.previous
-
+track.removeListitemById(4)
+track.outputList()
