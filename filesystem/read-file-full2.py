cabb507c9d5ca4aa90d19781ed364757fd7efc4c
Frank Hofmann <frank.hofmann@efho.de>
2018-02-24 19:59:02 +0100

    added examples of reading an entire file

---
+++ filesystem/read-file-full2.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------
+# read entire file content
+#
+# (C) 2018 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define the name of the file to read from
+filename = "test.txt"
+
+with open(filename, 'r') as filehandle:
+	filecontent = filehandle.readlines()
+	for line in filecontent:
+		print (line)
