880a500f400e228a44bf9015f1bf0a5a454c0312
Frank Hofmann <frank.hofmann@efho.de>
2018-02-25 23:48:37 +0100

    writing data to a file using redirection using the sys module

--- filesystem/write-file-redirect2.py
+++ filesystem/write-file-redirect2.py
@@ -1,23 +1,34 @@
 # -----------------------------------------------------------
 # write file content via redirection
-# works for Python 2.x
+# works for Python 2.x and 3.x
 #o
 # (C) 2018 Frank Hofmann, Berlin, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
 #
 # idea taken from:
-# https://stackoverflow.com/questions/4110891/python-how-to-simply-redirect-output-of-print-to-a-txt-file-with-a-new-line-crea
+# https://www.blog.pythonlibrary.org/2016/06/16/python-101-redirecting-stdout/
 # -----------------------------------------------------------
+
+# import sys module
+import sys
 
 # define the name of the output file
 filename = "helloworld.txt"
+
+# preserve the stdout channel
+original = sys.stdout
 
 # define content
 filecontent = ["Hello, world", "a second line", "and a third line"]
 
 with open(filename, 'w') as filehandle:
-	# redirect the output of print to the file handle
+	# set the new output channel
+	sys.stdout = filehandle
+	
 	for line in filecontent:
-		print >>filehandle, line
+		print(line)
+	
+	# restore the old output channel
+	sys.stdout = original
 
