d700025c8d44df3f3c6f1212d8aceb56ee8ff837
Frank Hofmann <frank.hofmann@efho.de>
2018-03-01 11:16:11 +0100

    printing the current working directory

---
+++ filesystem/cwd.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------
+# returns the current working directory
+#o
+# (C) 2018 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# import required python standard modules
+import os
+
+# detect the current working directory
+path = os.getcwd()
+print ("the current working directory is %s" % path)
