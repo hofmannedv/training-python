0271c3744f4ad7a051d33753b5dffe264024f6e3
Frank Hofmann <frank.hofmann@efho.de>
2018-04-29 20:59:28 +0200

    show the Zen of Python

---
+++ zen/zen.py
@@ -0,0 +1,11 @@
+# -----------------------------------------------------------
+# show the Zen of Python
+#o
+# (C) 2018 Frank Hofmann, Berlin, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# import required this module
+import this
+
