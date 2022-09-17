0d880a26df0c9804c4c10ca2424ef93153ba5020
Frank Hofmann <frank.hofmann@efho.de>
2022-09-17 19:17:48 +0200

    Evenly distributed values in a range

---
+++ data-processing/distributed-values.py
@@ -0,0 +1,23 @@
+# Python Data Science examples
+# (C) 2022 Frank Hofmann <frank.hofmann@efho.de>
+# License: GNU Public License (GPL)
+
+# create a list of equally distributed values within a value range
+
+# use numpy module under the local name np
+import numpy as np
+
+# value range from 2.0 to 3.0 (inclusive)
+# 11 float values
+valueList = np.linspace(2.0, 3.0, 11)
+print(valueList)                # 2.0, 2.1, ... 3.0
+
+# value range from 10 to 100 (inclusive)
+# 10 float values
+valueList = np.linspace(10, 100, 10)
+print(valueList)                # 10.0, 20.0, ... 100.0
+
+# value range from 10 to 100 (inclusive)
+# 10 integer values
+valueList = np.linspace(10, 100, num=10, dtype=int)
+print(valueList)                # 10, 20, ... 100
