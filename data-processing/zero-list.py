0d4f4967402ed1c6ed7002c10b358763af7f6756
Frank Hofmann <frank.hofmann@efho.de>
2022-09-17 19:55:29 +0200

    Create a list with zeros

---
+++ data-processing/zero-list.py
@@ -0,0 +1,18 @@
+# Python Data Science examples
+# (C) 2022 Frank Hofmann <frank.hofmann@efho.de>
+# License: GNU Public License (GPL)
+
+# create a list of zeros
+
+# use numpy module under the local name np
+import numpy as np
+
+# 5 zeros as float values
+valueList = np.zeros(5)
+print(valueList)                # [0.0, 0.0, 0.0, 0.0, 0.0]
+
+# 5 zeros as integer values
+valueList = np.zeros(5, int)
+print(valueList)                # [0, 0, 0, 0, 0]
+
+
