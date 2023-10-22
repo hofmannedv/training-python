e81145065c7a7c0476cb64132b42422b81db764b
Frank Hofmann <frank.hofmann@efho.de>
2023-10-22 18:28:34 +0200

    calculate area under function using trapz function from NumPy

---
+++ data-processing/calculate-area-of-function-numpy.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------
+# Python Data Science examples
+# calculate the size of a trapezoid (area) using numpy
+#o
+# (C) 2022-2023 Frank Hofmann, Germany
+# email frank.hofmann@efho.de
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+
+import numpy as np
+
+def calculateListOfTrapezoids():
+    """calculate the area for a list of data points"""
+
+    x = [1.0, 5.0, 9.0, 13.0]
+    y = [1.0, 3.0, 5.0, 7.0]
+
+    # initialize distance dx
+    dx = 3
+
+    # initialize the total size
+    totalSize = 0.0
+
+    # calculate size of area using trapz function from NumPy
+    totalSize = np.trapz(y, dx=dx)
+
+    return totalSize
+
+print(calculateListOfTrapezoids())
