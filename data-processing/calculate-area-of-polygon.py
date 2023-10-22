e97470fe485a4e4809b26020af919b0745388a83
Frank Hofmann <frank.hofmann@efho.de>
2023-10-22 11:47:27 +0200

    extend documentation, and add SPDX license identifier

--- data-processing/calculate-area-of-polygon.py
+++ data-processing/calculate-area-of-polygon.py
@@ -1,16 +1,24 @@
+# -----------------------------------------------------------
 # Python Data Science examples
-# (C) 2022 Frank Hofmann <frank.hofmann@efho.de>
-# License: GNU Public License (GPL)
-
 # calculate the size of a polygon (area)
+#o
+# (C) 2022-2023 Frank Hofmann, Germany
+# email frank.hofmann@efho.de
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
 
 def polyarea(coordinates):
+    "calculate the size of an polyarea"
+
+    # define size as 0
     size = 0
 
     # a = x1*y2 + x2*y3 + ... + xn-1*yn + xn*y1
     # b = y1*x2 + y2*x3 + ... + yn-1*xn + yn*x1
     # size = 1/2 * |(a - b)|
 
+    # identify the number of coordinates
     valueRange = range(len(coordinates))    # 0 to n - 1
 
     # calculate 1st component
@@ -36,11 +44,13 @@ def polyarea(coordinates):
 
     return size
 
+# example data for a triangle
 area = [
     [1.0, 1.0],
     [1.0, 3.0],
     [2.0, 1.0]
 ]
 
+# calculate size of triangle
 size = polyarea(area)
 print("The area has a size of %.2f cm^2" % size)
