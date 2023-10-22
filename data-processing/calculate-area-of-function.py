a293db910e4feaf6ee09aba7ff1bde5ff54984d7
Frank Hofmann <frank.hofmann@efho.de>
2023-10-22 16:35:15 +0200

    calculate different stripes

--- data-processing/calculate-area-of-function.py
+++ data-processing/calculate-area-of-function.py
@@ -29,39 +29,81 @@ def trapezoid(coordinates):
 
     return size
 
-# example data for a single trapezoid
-# coordinates: (0.0, 0.0) - (0.0, 2.0) - (1.0, 2.0) - (1.0, 0.0) 
-area = [
-    [0.0, 2.0],
-    [1.0, 2.0]
-]
+def calculateSingleTrapezoid():
+    """calculate the area for a single trapezoid
+       coordinates: 
+       (0.0, 0.0) - (0.0, 2.0) - (1.0, 2.0) - (1.0, 0.0)
+    """
 
-# calculate size of the trapezoid
-size = trapezoid(area)
-print("The area has a size of %.2f cm^2" % size)
+    area = [
+        [0.0, 2.0],
+        [1.0, 2.0]
+    ]
 
-# example data for a list of data points
-data = [
-    [0.0, 2.0],
-    [1.0, 2.0],
-    [2.0, 2.5],
-    [3.0, 3.0],
-    [4.0, 3.5],
-    [5.0, 2.0]
-]
-
-# define range (start-end) to be evaluated
-xStart = 1.0
-xEnd = 4.0
-
-# initialize the total size
-totalSize = 0.0
-
-# calculate each trapezoid, and the total size
-for position in range(len(data) - 1):
-    area = data[position:position + 2]
+    # calculate size of the trapezoid
     size = trapezoid(area)
-    print("area between", area, "has a size of", size, "cm^2")
-    totalSize += size
+    print("The area has a size of %.2f cm^2" % size)
 
-print("The area has a size of %.2f cm^2" % totalSize)
+    return
+
+def calculateListOfTrapezoids():
+    """calculate the area for a list of data points"""
+
+    data = [
+        [0.0, 2.0],
+        [1.0, 2.0],
+        [2.0, 2.5],
+        [3.0, 3.0],
+        [4.0, 3.5],
+        [5.0, 2.0]
+    ]
+
+    # initialize the total size
+    totalSize = 0.0
+
+    # calculate each trapezoid, and the total size
+    for position in range(len(data) - 1):
+        area = data[position:position + 2]
+        size = trapezoid(area)
+        print("area between", area, "has a size of", size, "cm^2")
+        totalSize += size
+
+    print("The area has a size of %.2f cm^2" % totalSize)
+
+    return
+
+def calculateTrapezoidFunction(xStart, xEnd, step):
+    """calculate the area for a given range for a function"""
+
+    # define underlying mathematical base function
+    # f(x) = x^2 - 4x + 5
+
+    # initialize the total size
+    totalSize = 0.0
+
+    # calculate the single trapezoids
+    for xPosition1 in range(xStart, xEnd, step):
+        xPosition2 = xPosition1 + step
+
+        yPosition1 = xPosition1**2 - 4 * xPosition1 + 5
+        yPosition2 = xPosition2**2 - 4 * xPosition2 + 5
+
+        position1 = [xPosition1, yPosition1]
+        position2 = [xPosition2, yPosition2]
+
+        # calculate the trapezoid
+        area = [position1, position2]
+        size = trapezoid(area)
+        # print("area between", area, "has a size of", size, "cm^2")
+
+        # calculate the total size 
+        totalSize += size
+
+    print("The area between %.2f und %.2f with step %.2f has an approximate size of %.2f cm^2" % (xStart, xEnd, step, totalSize))
+
+    return
+
+calculateSingleTrapezoid()
+calculateListOfTrapezoids()
+for step in range(1, 51, 1):
+    calculateTrapezoidFunction(0, 50, step)
