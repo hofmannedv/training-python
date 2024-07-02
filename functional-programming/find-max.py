cca97046f8fb85f30c5004bd711ea788a771edc5
Frank Hofmann <frank.hofmann@efho.de>
2024-07-02 19:35:06 +0200

    Functional programming: use max function

---
+++ functional-programming/find-max.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------
+# demonstrates finding the maximum value 
+#o
+# (C) 2024 Frank Hofmann, Freiburg, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# -----------------------------------------------------------
+
+# define a list of numbers
+numbers = [1, 3, 5, 6, 2]
+
+# apply max() to numbers
+print("The maximum element of the list is", max(numbers))
