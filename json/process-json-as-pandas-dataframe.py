154bfed0eb27ce87896f2ddde8c97aa5a8525533
Frank Hofmann <frank.hofmann@efho.de>
2024-08-13 18:52:49 +0200

    process JSON data as Pandas dataframe

---
+++ json/process-json-as-pandas-dataframe.py
@@ -0,0 +1,18 @@
+# Python Data Science examples
+# (C) 2024 Frank Hofmann <frank.hofmann@efho.de>
+# License: GNU Public License (GPL)
+
+# load a JSON data file into a Pandas dataframe
+
+# use Pandas, and JSON module
+import pandas as pd
+import json
+
+# define name of the JSON data file
+data = "data.json"
+
+# load data into dataframe
+df = pd.read_json(data)
+
+# output dataframe 
+print(df)
