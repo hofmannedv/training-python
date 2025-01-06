6e80bc8fb227ee45263b43551249ea8b7ec7cf4e
Frank Hofmann <frank.hofmann@efho.de>
2025-01-06 10:46:32 +0100

    detect min and max value in a list of 1d points

---
+++ points-and-ranges/detect-point-range-1d.py
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------
+# algorithms for data points
+# demonstrates how to determine the smallest, and biggest 
+# value in a one-dimensional list of floats
+#o
+# (C) 2025 Frank Hofmann, Germany
+# email frank.hofmann@efho.de
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+
+# define example data: points
+points = [3.5, 17.2, 6.3, 9.15, 4.2]
+
+# determine the minimal value
+minimal = min(points)
+
+# determine the maximal value
+maximal = max(points)
+
+print(f"the points range from {minimal} to {maximal}")
+
