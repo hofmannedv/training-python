d6b5b6a2c26c5961ae38366005fe7f3924bd57af
Frank Hofmann <frank.hofmann@efho.de>
2025-01-12 19:30:56 +0100

    add SPDX identifier

--- subprocesses/diskfree-grab-output.py
+++ subprocesses/diskfree-grab-output.py
@@ -4,9 +4,10 @@
 #
 # requires Python 3.5
 #
-# (C) 2017 Frank Hofmann, Berlin, Germany
-# Released under GNU Public License (GPL)
+# (C) 2017-2025 Frank Hofmann, Germany
 # email frank.hofmann@efho.de
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # include standard modules
