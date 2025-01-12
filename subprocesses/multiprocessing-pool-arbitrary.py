8eba90ab8371dd34bf8a81bcbb1a0e3fde9efe58
Frank Hofmann <frank.hofmann@efho.de>
2025-01-12 19:36:55 +0100

    add SPDX identifier

--- subprocesses/multiprocessing-pool-arbitrary.py
+++ subprocesses/multiprocessing-pool-arbitrary.py
@@ -6,9 +6,10 @@
 # idea taken from this tutorial:
 # https://docs.python.org/3/library/multiprocessing.html
 #
-# (C) 2017 Frank Hofmann, Berlin, Germany
-# Released under GNU Public License (GPL)
+# (C) 2017-2025 Frank Hofmann, Germany
 # email frank.hofmann@efho.de
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # include standard modules
