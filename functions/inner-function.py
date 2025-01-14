10c62ea038a6175da6a0aa24bdee037d62fcf2a1
Frank Hofmann <frank.hofmann@efho.de>
2025-01-14 22:42:33 +0100

    added 2nd example for inner function, or closure

--- functions/inner-function.py
+++ functions/inner-function.py
@@ -8,12 +8,23 @@
 # -----------------------------------------------------------
 
 def increment (number):                        	# start of the outer function
+    """increment by one"""
     def innerIncrement ():                  	# start of the inner function
         result = number + 1            		# process a new value
         return result                      	# return result of the calculation
     return innerIncrement()                    	# end of closure
 
+def incrementTwo (number, value):              	# start of the outer function
+    """increment by value"""
+    def innerIncrement (value):                	# start of the inner function
+        result = number + value        		# process a new value
+        return result                      	# return result of the calculation
+    return innerIncrement(value)               	# end of closure
+
 # output result
 print("the increment of 0 is", increment(0))	# call 1
 print("the increment of 1 is", increment(1))	# call 2
 print("the increment of 2 is", increment(2))	# call 3
+print("the increment of 0 by 5 is", incrementTwo(0, 5))	# call 1
+print("the increment of 1 by 10 is", incrementTwo(1, 10))	# call 2
+print("the increment of 2 by 4 is", incrementTwo(2, 4))	# call 3
