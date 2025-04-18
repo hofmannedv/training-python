9bd5fbcf5f63c09b30f5cac51b985a0d6f00d303
Frank Hofmann <frank.hofmann@efho.de>
2025-04-18 23:56:17 +0200

    added route class example with comparison

--- route-tracking/route-berechnen.py
+++ route-tracking/route-berechnen.py
@@ -2,30 +2,48 @@ from route2d import Route2d
 from strecke2d import Strecke2d
 from punkt2d import Punkt2d
 
-listeDerWegpunkte = [
+def berechneDistanz(listeDerWegpunkte):
+    distanz = 0
+
+    listeDerStrecken = []
+
+    position = 0
+    while position < len(listeDerWegpunkte) - 1:
+        startpunkt = Punkt2d(
+            listeDerWegpunkte[position][0],
+            listeDerWegpunkte[position][1],
+            listeDerWegpunkte[position][2]
+        ) 
+        endpunkt = Punkt2d(
+            listeDerWegpunkte[position + 1][0],
+            listeDerWegpunkte[position + 1][1],
+            listeDerWegpunkte[position + 1][2]
+        )
+        strecke = Strecke2d("Strecke %i" % (position + 1), startpunkt, endpunkt)
+        listeDerStrecken.append(strecke)
+
+        position = position + 1
+
+    route = Route2d("Route 1", listeDerStrecken)
+    distanz = route.getLaenge()
+    return distanz
+
+listeDerWegpunkte1 = [
     ["A", 1, 1],
     ["B", 2, 2],
     ["C", 3, 2]
 ]
-listeDerStrecken = []
+listeDerWegpunkte2 = [
+    ["A", 1, 1],
+    ["D", 2, 3],
+    ["C", 3, 2]
+]
+distanz1 = berechneDistanz(listeDerWegpunkte1)
+distanz2 = berechneDistanz(listeDerWegpunkte2)
+print("Länge der Route 1:", distanz1)
+print("Länge der Route 2:", distanz2)
 
-position = 0
-while position < len(listeDerWegpunkte) - 1:
-    startpunkt = Punkt2d(
-        listeDerWegpunkte[position][0],
-        listeDerWegpunkte[position][1],
-        listeDerWegpunkte[position][2]
-    ) 
-    endpunkt = Punkt2d(
-        listeDerWegpunkte[position + 1][0],
-        listeDerWegpunkte[position + 1][1],
-        listeDerWegpunkte[position + 1][2]
-    )
-    strecke = Strecke2d("Strecke %i" % (position + 1), startpunkt, endpunkt)
-    listeDerStrecken.append(strecke)
-
-    position = position + 1
-
-route = Route2d("Route 1", listeDerStrecken)
-route.info()
-print("Länge der Route:", route.getLaenge())
+kuerzesteRoute = 1
+if distanz2 < distanz1:
+    kuerzesteRoute = 2
+print("die kürzeste Route ist Route", kuerzesteRoute)
