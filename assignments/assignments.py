98fd8b9553536b52972c75667650e05fdacb199f
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 17:32:47 +0200

    add license information: GPL as SPDX

--- assignments/assignments.py
+++ assignments/assignments.py
@@ -1,14 +1,17 @@
 # -----------------------------------------------------------
 # demonstrates different kind of assignments
 #o
-# (C) 2014 Frank Hofmann, Berlin, Germany
+# (C) 2014-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # single assignments
 colour = "blue"					# string assignment
-size = 17						# integer assignment
+size = 17					# integer assignment
 length = 2.5					# float value assignment
 
 print ("colour:", colour)
@@ -17,15 +20,15 @@ print ("length:", length)
 
 # multiple assignments
 a = b = c = 1					# set a, b, and c to 1
-d,e = 2, 3						# set d to 2, and e to 3
+d,e = 2, 3					# set d to 2, and e to 3
 print (("a,b,c : %i,%i,%i") % (a,b,c))
 print (("d,e   : %i,%i") % (d,e))
 
 # assignments with calculations
-total = a + b - c + d -e		# simple addition
+total = a + b - c + d -e			# simple addition
 output = colour * 4				# concatenate a string four times
-output2 = "light" + colour		# concatenate two strings
-output3 = colour + str(total)	# concatenate a string with converted integer
+output2 = "light" + colour			# concatenate two strings
+output3 = colour + str(total)			# concatenate a string with converted integer
 print (("total : %i") % (total))
 print (("colour: %s") % (colour))
 print (("output: %s") % (output))
