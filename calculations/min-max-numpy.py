173e915fa6ba63cc85a53fae56d6ade6889371b2
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 19:05:40 +0200

    add license information: GPL as SPDX

--- calculations/min-max-numpy.py
+++ calculations/min-max-numpy.py
@@ -2,9 +2,12 @@
 # calculate the minimum, and the maximum value for a list of integers
 # using the NumPy module
 #o
-# (C) 2016 Frank Hofmann, Berlin, Germany
+# (C) 2016-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # requirements:
