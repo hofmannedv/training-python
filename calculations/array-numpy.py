adc97a6087b0adfa7930044ae11f2d801b0f43fe
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 18:55:39 +0200

    add license information: GPL as SPDX

--- calculations/array-numpy.py
+++ calculations/array-numpy.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates various array calculations using the NumPy module
 # 
-# (C) 2016 Frank Hofmann, Berlin, Germany
+# (C) 2016-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # requirements:
