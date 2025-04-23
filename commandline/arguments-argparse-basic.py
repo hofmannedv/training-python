e5597e0d03171cd4d3df2d2ae0796d5d290204b3
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 19:10:25 +0200

    add license information: GPL as SPDX

--- commandline/arguments-argparse-basic.py
+++ commandline/arguments-argparse-basic.py
@@ -2,9 +2,12 @@
 # demonstrates how to work with commandline arguments
 # using the argparse module (available from Python 3.2)
 #o
-# (C) 2017 Frank Hofmann, Berlin, Germany
+# (C) 2017-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # include standard modules
