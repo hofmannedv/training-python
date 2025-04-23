58e6b273064ec8234d631fabfa4899bc25b50266
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 19:01:27 +0200

    add license information: GPL as SPDX

--- calculations/calc2.py
+++ calculations/calc2.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # calculate the total for a list of items
 # 
-# (C) 2014 Frank Hofmann, Berlin, Germany
+# (C) 2014-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a list of single values
