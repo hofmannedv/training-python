58a0a4ea5f2a3c9c79550a32eb903a7176031187
Frank Hofmann <frank.hofmann@efho.de>
2025-04-23 17:39:40 +0200

    add assignment/replacement using f-strings

--- assignments/assignments.py
+++ assignments/assignments.py
@@ -21,11 +21,11 @@ print ("length:", length)
 # multiple assignments
 a = b = c = 1					# set a, b, and c to 1
 d,e = 2, 3					# set d to 2, and e to 3
-print (("a,b,c : %i,%i,%i") % (a,b,c))
-print (("d,e   : %i,%i") % (d,e))
+print (("a,b,c : %i,%i,%i") % (a,b,c))		# replace values using C notation
+print (f"d,e   : {d},{e}")			# replace values using f-strings
 
 # assignments with calculations
-total = a + b - c + d -e			# simple addition
+total = a + b - c + d - e			# simple addition
 output = colour * 4				# concatenate a string four times
 output2 = "light" + colour			# concatenate two strings
 output3 = colour + str(total)			# concatenate a string with converted integer
@@ -35,6 +35,6 @@ print (("output: %s") % (output))
 print (("output2: %s") % (output2))
 print (("output3: %s") % (output3))
 
-# print a partial string
+# print a partial string from index position 2 up to, but not including 4
 partialString = output2[2:4]
-print ("partialString:", partialString)
+print ("partialString:", partialString)		# output: gh
