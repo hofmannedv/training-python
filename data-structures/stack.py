3280fba8b524682165bfe8e12932ab6891cb3a3e
Frank Hofmann <frank.hofmann@efho.de>
2025-04-24 17:44:19 +0200

    finetuning, add license information: GPL as SPDX

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # create a stack class
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 #
 
@@ -36,29 +39,30 @@ class Stack:
 		return len(self.items)
 
 # main program
-stack = Stack()
+if __name__ == '__main__':
+	stack = Stack()
+	
+	# define data
+	data = [15, 7, [4, 23], 39]
+	
+	# go through it step by step
+	for item in data:
+		if isinstance(item, int):
+			# we have an integer
+			stack.push(item)
+		elif isinstance(item, list):
+			# we have a list of integers
+			for listItem in item:
+				stack.push(listItem)
+		
+	# count total
+	total = 0
 
-# define data
-data = [15, 7, [4, 23], 39]
+	# take the items from the stack
+	while not stack.isEmpty():
+		value = stack.pop()
+		print ("added %i to %i" % (value, total))
+		total += value
 
-# go through it step by step
-for item in data:
-	if isinstance(item, int):
-		# we have an integer
-		stack.push(item)
-	elif isinstance(item, list):
-		# we have a list of integers
-		for listItem in item:
-			stack.push(listItem)
-
-# count total
-total = 0
-
-# take the items from the stack
-while not stack.isEmpty():
-	value = stack.pop()
-	print ("added %i to %i" % (value, total))
-	total += value
-
-# output total
-print ("total:", total)
+	# output total
+	print ("total:", total)
