5c95af87f72aa4be3de09c084e743dd99e9e6171
Frank Hofmann <frank.hofmann@efho.de>
2025-04-24 17:51:08 +0200

    finetuning, add license information: GPL as SPDX

--- data-structures/validate-object-instance.py
+++ data-structures/validate-object-instance.py
@@ -2,9 +2,12 @@
 # demonstrates how to validate a variable for being an object
 # inherited from a certain class
 #o
-# (C) 2017 Frank Hofmann, Berlin, Germany
+# (C) 2017-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 class Point:
