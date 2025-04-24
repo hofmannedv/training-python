b02d8a124bd7106beefdd0198064b4a487b74fde
Frank Hofmann <frank.hofmann@efho.de>
2025-04-24 17:55:04 +0200

    add license information: GPL as SPDX

--- data-structures/read-list-from-file.py
+++ data-structures/read-list-from-file.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates how to read a list from a file
 #o
-# (C) 2018 Frank Hofmann, Berlin, Germany
+# (C) 2018-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define empty list
