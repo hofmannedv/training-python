8efc4c705bf2541c26ed7ad95740be75352c405e
Frank Hofmann <frank.hofmann@efho.de>
2025-04-25 16:45:14 +0200

    add license information: GPL as SPDX

--- inotify/observe-directory.py
+++ inotify/observe-directory.py
@@ -5,6 +5,9 @@
 # (C) 2025 Frank Hofmann, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 #
 # inspired by this post on Lowendbox:
 # https://lowendbox.com/blog/instant-reactions-to-filesystem-events-with-inotify/
