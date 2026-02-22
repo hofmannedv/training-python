bf449e7d0541f245028c2506929b3c5981965e98
Frank Hofmann <frank.hofmann@efho.de>
2026-02-22 17:41:00 +0100

    Erweiterung um Punkt2d-Klasse

--- route-tracking/route-berechnen.py
+++ route-tracking/route-berechnen.py
@@ -2,7 +2,7 @@
 # demonstriert die Berechnung der Weglänge für einen Weg im 
 # zweidimensionalen Raum
 #o
-# (C) 2025 Frank Hofmann, Freiburg, Germany
+# (C) 2025-2026 Frank Hofmann, Freiburg, Germany
 # email frank.hofmann@efho.de
 # License: GNU Public License (GPL) v.3.0 or later
 # SPDX-License-Identifier: GNU General Public License v3.0 or later
@@ -13,22 +13,15 @@ from strecke2d import Strecke2d
 from punkt2d import Punkt2d
 
 def berechneDistanz(listeDerWegpunkte):
+    """ berechne die Distanz einer Route anhand von Wegpunkten """
     distanz = 0
 
     listeDerStrecken = []
 
     position = 0
     while position < len(listeDerWegpunkte) - 1:
-        startpunkt = Punkt2d(
-            listeDerWegpunkte[position][0],
-            listeDerWegpunkte[position][1],
-            listeDerWegpunkte[position][2]
-        ) 
-        endpunkt = Punkt2d(
-            listeDerWegpunkte[position + 1][0],
-            listeDerWegpunkte[position + 1][1],
-            listeDerWegpunkte[position + 1][2]
-        )
+        startpunkt = listeDerWegpunkte[position]
+        endpunkt = listeDerWegpunkte[position + 1]
         strecke = Strecke2d("Strecke %i" % (position + 1), startpunkt, endpunkt)
         listeDerStrecken.append(strecke)
 
@@ -39,11 +32,22 @@ def berechneDistanz(listeDerWegpunkte):
     return distanz
 
 def ermittleWegpunkte(listeDerWegpunkte):
+    """ berechne die Route anhand der Namen der Wegpunkte """
     wegpunkte = []
     for punkt in listeDerWegpunkte:
-        name = punkt[0]
+        name = punkt.getBezeichnung()
         wegpunkte.append(name)
     return wegpunkte
+
+# definiere vier Wegpunkte A bis D
+wegpunkt1 = Punkt2d("A", 1, 1)
+wegpunkt2 = Punkt2d("B", 2, 2)
+wegpunkt3 = Punkt2d("C", 3, 2)
+wegpunkt4 = Punkt2d("D", 2, 3)
+
+# definiere zwei Routen
+route1 = [wegpunkt1, wegpunkt2, wegpunkt3]
+route2 = [wegpunkt1, wegpunkt4, wegpunkt3]
 
 listeDerWegpunkte1 = [
     ["A", 1, 1],
@@ -57,12 +61,12 @@ listeDerWegpunkte2 = [
 ]
 
 # Länge der Routen berechnen
-distanz1 = berechneDistanz(listeDerWegpunkte1)
-distanz2 = berechneDistanz(listeDerWegpunkte2)
+distanz1 = berechneDistanz(route1)
+distanz2 = berechneDistanz(route2)
 
 # Wegpunkte der Routen ermitteln
-wegpunkte1 = ermittleWegpunkte(listeDerWegpunkte1)
-wegpunkte2 = ermittleWegpunkte(listeDerWegpunkte2)
+wegpunkte1 = ermittleWegpunkte(route1)
+wegpunkte2 = ermittleWegpunkte(route2)
 
 # Ausgabe vorbereiten
 routenverlauf1 = "(" + " - ".join(wegpunkte1) + ")"
@@ -74,4 +78,4 @@ print("Länge der Route 2 %s: %f" % (routenverlauf2, distanz2))
 kuerzesteRoute = 1
 if distanz2 < distanz1:
     kuerzesteRoute = 2
-print("die kürzeste Route ist Route", kuerzesteRoute)
+print("die kürzere Route ist Route", kuerzesteRoute)
