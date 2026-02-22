6fa45a70f181964301ded28119dd02dd0992f130
Frank Hofmann <frank.hofmann@efho.de>
2026-02-22 19:25:53 +0100

    Umbau auf networkx-Bibliothek

--- route-tracking/route-ermitteln.py
+++ route-tracking/route-ermitteln.py
@@ -1,9 +1,8 @@
 # -----------------------------------------------------------
 # demonstriert die Ermittlung einer Route in einem Graphen im
-# zweidimensionalen Raum
+# zweidimensionalen Raum mit Hilfe der Bibliothek networkx
+# (networkx: https://pypi.org/project/networkx/)
 #
-# work in progress
-# 
 # (C) 2025-2026 Frank Hofmann, Freiburg, Germany
 # email frank.hofmann@efho.de
 # License: GNU Public License (GPL) v.3.0 or later
@@ -14,187 +13,47 @@ from route2d import Route2d
 from strecke2d import Strecke2d
 from punkt2d import Punkt2d
 
-def berechneDistanz(listeDerWegpunkte):
-    """ berechne die Distanz einer Route anhand von Wegpunkten """
-    distanz = 0
-
-    listeDerStrecken = []
-
-    position = 0
-    while position < len(listeDerWegpunkte) - 1:
-        startpunkt = listeDerWegpunkte[position]
-        endpunkt = listeDerWegpunkte[position + 1]
-        strecke = Strecke2d("Strecke %i" % (position + 1), startpunkt, endpunkt)
-        listeDerStrecken.append(strecke)
-
-        position = position + 1
-
-    route = Route2d("Route 1", listeDerStrecken)
-    distanz = route.getLaenge()
-    return distanz
-
-    distanz = 0
-
-def ermittleWegpunkte(listeDerWegpunkte):
-    """ berechne die Route anhand der Namen der Wegpunkte """
-    wegpunkte = []
-    for punkt in listeDerWegpunkte:
-        name = punkt.getBezeichnung()
-        wegpunkte.append(name)
-    return wegpunkte
-
-def ermittleGraphstruktur(streckenliste, ausgangspunkt, zielpunkt):
-    """ ermittle eine Graphstruktur zwischen Start- und Zielpunkt """
-    ergebnis = []
-    weitereRouten = []
-
-    if streckenliste != []:
-        testliste = []
-        for eintrag in streckenliste:
-            startpunkt = eintrag.getWegpunkt1().getBezeichnung()
-            if startpunkt == ausgangspunkt:
-                testliste.append(eintrag)
-
-        print("Testliste:", testliste)
-
-        bereinigteStreckenliste = []
-        for teilstrecke in streckenliste:
-            if teilstrecke not in testliste:
-                bereinigteStreckenliste.append(teilstrecke)
-
-        print("bereinigte Streckenliste:", bereinigteStreckenliste)
-
-        for teilstrecke in testliste:
-            #startpunkt = teilstrecke[0]
-            endpunkt = teilstrecke.getWegpunkt2().getBezeichnung()
-            if endpunkt == zielpunkt:
-                ergebnis.append(teilstrecke)
-                break
-            else:
-                print("suche nach Teilstrecken beginnend mit", endpunkt)
-                weitereRouten = ermittleGraphstruktur(bereinigteStreckenliste, endpunkt, zielpunkt)
-                print("gefunden: ", weitereRouten)
-
-                if weitereRouten:
-                    zwischenergebnis = [teilstrecke]
-
-                    if len(weitereRouten) == 1:
-                        zwischenergebnis.append(weitereRouten[0])
-                    else:
-                        zwischenergebnis.append(weitereRouten)
-                    ergebnis.append(zwischenergebnis)
-
-    return ergebnis
-
-def ermittleRouten(graphstruktur):
-    print("aufgerufen mit Graphstruktur:", graphstruktur)
-    # print("aufgerufen mit Streckenliste:", streckenliste)
-    ergebnis = []
-
-    print("prüfe Graphstruktur:", graphstruktur)
-    if isinstance(graphstruktur, Strecke2d):
-        ergebnis.append(graphstruktur)
-    else:
-        for eintrag in graphstruktur:
-            print("prüfe Eintrag", eintrag)
-            if isinstance(eintrag, Strecke2d):
-                ergebnis.append(eintrag)
-
-            if isinstance(eintrag, list):
-                print("prüfe Varianten aus:", eintrag)
-                for variante in eintrag:
-                    print("prüfe Variante:", variante)
-                    gefundeneRouten = ermittleRouten(variante)
-                    neueRouten = []
-                    for unterRoute in gefundeneRouten:
-                        print("füge Variante hinzu:", [variante, unterRoute])
-                        ergebnis.append([variante, unterRoute])
-
-    return ergebnis
+import networkx as nx
 
 # lege die Wegpunkte fest [Name, x-Koordinate, y-Koordinate]
-listeDerWegpunkte = [
-    ["A", 1, 1],
-    ["B", 2, 2],
-    ["C", 3, 2],
-    ["D", 2, 3],
-    ["E", 4, 0],
-    ["F", 5, 0],
-    ["Z", 0, 0]
-]
+wegpunktA = Punkt2d("A", 1, 1)
+wegpunktB = Punkt2d("B", 2, 2)
+wegpunktC = Punkt2d("C", 3, 2)
+wegpunktD = Punkt2d("D", 3, 3)
+wegpunktE = Punkt2d("E", 4, 0)
+wegpunktF = Punkt2d("F", 5, 0)
+wegpunktZ = Punkt2d("Z", 0, 0)
+listeDerWegpunkte = [wegpunktA, wegpunktB, wegpunktC, wegpunktD, wegpunktE, wegpunktF, wegpunktZ]
 
 # übertrage die Wegpunkte in Objekte der Klasse Punkt2d
-listeDerWegpunkte2 = []
-for datensatz in listeDerWegpunkte:
-    wegpunkt = Punkt2d(datensatz[0], datensatz[1], datensatz[2])
+for wegpunkt in listeDerWegpunkte:
     print("erzeuge Wegpunkt:")
     wegpunkt.info()
-    listeDerWegpunkte2.append(wegpunkt)
+
+# definiere Streckennetz als leeren Graphen
+streckennetz = nx.Graph()
 
 # lege die Teilstrecken fest: [Startpunkt, Endpunkt]
-listeDerTeilstrecken = [
-    ["A", "B"],
-    ["B", "C"],
-    ["B", "D"],
-    ["C", "E"],
-    ["D", "E"],
-    ["E", "F"],
-    ["Z", "A"]
-]
+teilstrecke1 = Strecke2d("Teilstrecke 1", wegpunktA, wegpunktB)
+teilstrecke2 = Strecke2d("Teilstrecke 2", wegpunktB, wegpunktC)
+teilstrecke3 = Strecke2d("Teilstrecke 3", wegpunktB, wegpunktD)
+teilstrecke4 = Strecke2d("Teilstrecke 4", wegpunktC, wegpunktE)
+teilstrecke5 = Strecke2d("Teilstrecke 5", wegpunktD, wegpunktE)
+teilstrecke6 = Strecke2d("Teilstrecke 6", wegpunktE, wegpunktF)
+teilstrecke7 = Strecke2d("Teilstrecke 7", wegpunktZ, wegpunktA)
 
-# übertrage die Teilstrecken in Objekte der Klasse Strecke2d
-listeDerTeilstrecken2 = []
-for datensatz in listeDerTeilstrecken:
-    startpunkt = datensatz[0]
-    zielpunkt = datensatz[1]
+listeDerTeilstrecken = [teilstrecke1, teilstrecke2, teilstrecke3, teilstrecke4, teilstrecke5, teilstrecke6, teilstrecke7]
 
-    streckenanfang = None
-    streckenende = None
-    for eintrag in listeDerWegpunkte2:
-        if startpunkt == eintrag.getBezeichnung():
-            streckenanfang = eintrag
-            break
-    for eintrag in listeDerWegpunkte2:
-        if zielpunkt == eintrag.getBezeichnung():
-            streckenende = eintrag
-            break
-    teilstrecke = Strecke2d("Teilstrecke", streckenanfang, streckenende)
+for teilstrecke in listeDerTeilstrecken:
     print("lege neue Teilstrecke an:")
     teilstrecke.info()
-    listeDerTeilstrecken2.append(teilstrecke)
 
-#listeDerRouten = []
+    # füge jede Teilstrecke dem Streckennetz hinzu, Gewicht: 1
+    bezeichnung = teilstrecke.getBezeichnung()
+    von = teilstrecke.getWegpunkt1().getBezeichnung()
+    nach = teilstrecke.getWegpunkt2().getBezeichnung()
+    streckennetz.add_edge(von, nach, weight="1")
 
-
-# Länge der Routen berechnen
-# distanz1 = berechneDistanz(listeDerWegpunkte1)
-# distanz2 = berechneDistanz(listeDerWegpunkte2)
-
-# Wegpunkte der Routen ermitteln
-# wegpunkte1 = ermittleWegpunkte(listeDerWegpunkte1)
-# wegpunkte2 = ermittleWegpunkte(listeDerWegpunkte2)
-
-# Ausgabe vorbereiten
-# routenverlauf1 = "(" + " - ".join(wegpunkte1) + ")"
-# routenverlauf2 = "(" + " - ".join(wegpunkte2) + ")"
-# print("Länge der Route 1 %s: %f" % (routenverlauf1, distanz1))
-# print("Länge der Route 2 %s: %f" % (routenverlauf2, distanz2))
-
-# Kürzeste Route bestimmen (Annahme: Route 1)
-# kuerzesteRoute = 1
-# if distanz2 < distanz1:
-#     kuerzesteRoute = 2
-# print("die kürzeste Route ist Route", kuerzesteRoute)
-graph = ermittleGraphstruktur(listeDerTeilstrecken2, "A", "C")
-print(graph)
-
-# for eintrag in graph:
-#     if isinstance(eintrag, Strecke2d):
-#         print(eintrag.getWegpunkt1().getBezeichnung(), "->", eintrag.getWegpunkt2().getBezeichnung())
-#     else:
-#    	print(eintrag)
-
-# listeDerRouten = ermittleRouten(graph)
-# print(listeDerRouten)
-#for einzelneRoute in listeDerRouten:
-#    print("gefundene Route:", einzelneRoute)
+# ermittle kürzesten Pfad von Wegpunkt A nach D
+pfad = nx.shortest_path(streckennetz, "A", "D")
+print("gefundener Weg von A nach D:", pfad)
