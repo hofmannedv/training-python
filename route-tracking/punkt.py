32031cc5b5df4a0651d3f5a52583fa30026dd09d
Frank Hofmann <frank.hofmann@efho.de>
2026-02-22 17:08:45 +0100

    Docstrings zu den Methoden ergänzt

--- route-tracking/punkt.py
+++ route-tracking/punkt.py
@@ -2,7 +2,7 @@
 # demonstriert eine Klasse Punkt für einen Punkt im 
 # eindimensionalen Raum
 #o
-# (C) 2024-2025 Frank Hofmann, Freiburg, Germany
+# (C) 2024-2026 Frank Hofmann, Freiburg, Germany
 # email frank.hofmann@efho.de
 # License: GNU Public License (GPL) v.3.0 or later
 # SPDX-License-Identifier: GNU General Public License v3.0 or later
@@ -12,20 +12,26 @@ class Punkt:
   bezeichnung = ""
 
   def __init__(self, bezeichnung):
-    self.bezeichnung = bezeichnung
+    """ initialisiere das Objekt mit dem übergebenen Wert """
+    self.setBezeichnung(bezeichnung)
     return
 
   def setBezeichnung(self, bezeichnung):
+    """ setze die Bezeichnung des Punkts auf den übergebenen Wert """
     self.bezeichnung = bezeichnung
     return
     
   def getBezeichnung(self):
+    """ gebe die gespeicherte Bezeichnung des Punkts zurück """
     return self.bezeichnung
     
   def hasBezeichnung(self):
+    """ prüfe, ob die gespeicherte Bezeichnung leer ist """
     return self.bezeichnung != ""
     
   def compareBezeichnung(self, bezeichnung):
+    """ prüfe, ob die übergebene Bezeichnung mit der gespeicherten 
+        Bezeichnung des Punktes übereinstimmt """
     return self.bezeichnung == bezeichnung
 
 if __name__ == '__main__':
