21d5b128ea1d49b2933f67d3ae41ff63cac1c259
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:22:48 +0100

    added reversing lists, with SPDX license identifier

--- lists-dictionaries-tuples/list-unique.py
+++ lists-dictionaries-tuples/list-unique.py
@@ -4,6 +4,9 @@
 # (C) 2015 Frank Hofmann, Berlin, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # ideas are taken from Peter Bengtsson's blog:
