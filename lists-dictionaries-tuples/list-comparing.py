a93c19f2811e743d2272dc5091ba0ded436b2843
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 15:01:42 +0100

    comparing lists using cmp, added SPDX license identifier, and fixed bug

--- lists-dictionaries-tuples/list-comparing.py
+++ lists-dictionaries-tuples/list-comparing.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates how to compare lists element by element
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # NOTE: works for Python 2.x, only
