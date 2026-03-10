4aa157921337f8ca733a64e9b11904b2ad5e3348
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:59:24 +0100

    combining an iterable using zip, added SPDX license identifier, and fixed bug

--- lists-dictionaries-tuples/iterable-zip.py
+++ lists-dictionaries-tuples/iterable-zip.py
@@ -1,11 +1,12 @@
-# -*- coding: utf-8 -*-
-
 # -----------------------------------------------------------
 # demonstrates how to use the zip function
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define variables zipCode, place
@@ -18,6 +19,6 @@ print (address)
 
 # output address
 for item in address:
-	print ("ZIP code: %s" % item[0])
-	print ("place   : %s" % item[1])
-	print (" ")
+    print ("ZIP code: %s" % item[0])
+    print ("place   : %s" % item[1])
+    print (" ")
