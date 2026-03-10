85ac3590bba8c79c06aeee1beeecfe4d3f8c3cd2
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:41:18 +0100

    actions on iterables using map, added SPDX license identifier

--- lists-dictionaries-tuples/iterable-map.py
+++ lists-dictionaries-tuples/iterable-map.py
@@ -1,14 +1,17 @@
 # -----------------------------------------------------------
 # demonstrates how to use the map function
 #
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a simple function 
 def powerValue(operand):
-	return operand * operand
+    return operand * operand
 
 # define a list of operands
 operandList1 = [2.0, 4.0, 2.5]
@@ -17,16 +20,16 @@ operandList2 = (1.0, 2.0, 433.5)
 # output result
 for currentList in (operandList1, operandList2):
 
-	# start with first element at position #0
-	currentPosition = 0
+    # start with first element at position #0
+    currentPosition = 0
 
-	# call powerValue for each item from operand list
-	results = map(powerValue, currentList)
+    # call powerValue for each item from operand list
+    results = map(powerValue, currentList)
 
-	# output results
-	for i in results:
-		# output a float value with 2 digits
-		print("%8.2f:%10.2f" % (currentList[currentPosition],i))
+    # output results
+    for i in results:
+        # output a float value with 2 digits
+        print("%8.2f:%10.2f" % (currentList[currentPosition],i))
 
-		# increase current position
-		currentPosition += 1
+        # increase current position
+        currentPosition += 1
