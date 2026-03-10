cdee33a8b1eeba1d6060568b925f5ab8d8d971a1
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 15:10:26 +0100

    list number of items in lists, added SPDX license identifier, and fixed bug

--- lists-dictionaries-tuples/list-items.py
+++ lists-dictionaries-tuples/list-items.py
@@ -1,17 +1,20 @@
 # -----------------------------------------------------------
 # demonstrates how to extract the items of a dictionary using items()
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define an array (dictionary)
 capital = {
-	"France": "Paris",
-	"Switzerland": "Bern",
-	"Germany": "Berlin"
-	}
+    "France": "Paris",
+    "Switzerland": "Bern",
+    "Germany": "Berlin"
+}
 
 # output dictionary content (key-value-pairs)
 print (capital)
@@ -20,5 +23,5 @@ print (capital)
 entries = capital.items()
 
 # count the items
-print("#items:", len(entries))
+print("number of items:", len(entries))
 print(entries)
