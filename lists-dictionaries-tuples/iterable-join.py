2ec74315d72bf706707f2732b2495f4bd98a9cd9
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:39:22 +0100

    join iterables by a string, added SPDX license identifier

--- lists-dictionaries-tuples/iterable-join.py
+++ lists-dictionaries-tuples/iterable-join.py
@@ -1,13 +1,16 @@
 # -----------------------------------------------------------
 # demonstrates how to use the string join function
 #
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a list of string variables as a list
 names = ["Berlin", "New York", "Cape Town"]
 
 # print the list of places as a comma-separated list
-print ",".join(names)
+print(", ".join(names))
