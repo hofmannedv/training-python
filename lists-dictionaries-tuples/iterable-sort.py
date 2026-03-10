3540788970b6008cdc753b7cf35f8ad22c0d65c0
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:52:35 +0100

    sorting an iterables using sorted, added SPDX license identifier, and fixed bug

--- lists-dictionaries-tuples/iterable-sort.py
+++ lists-dictionaries-tuples/iterable-sort.py
@@ -2,30 +2,33 @@
 # demonstrates how to sort a dictionary by keys with the help of
 # iterators
 #
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a generator function
 def sortedDictGenerator(dictionary):
 
-	# retrieve the keys
-	dictionaryKeys = dictionary.keys()
+    # retrieve the keys as a list
+    dictionaryKeys = list(dictionary.keys())
 
-	# sort the keys in place
-	dictionaryKeys.sort()
+    # sort the keys in place
+    sortedKeys = sorted(dictionaryKeys)
 
-	# return the current key as an iterator
-	for currentKey in dictionaryKeys:
-		# return a single element, only
-		yield currentKey
+    # return the current key as an iterator
+    for currentKey in sortedKeys:
+        # return a single element, only
+        yield currentKey
 
 # define a dictionary of capitals
 capitals = {
-	"Norway": "Oslo",
-	"Germany": "Berlin",
-	"France": "Paris"
+    "Norway": "Oslo",
+    "Germany": "Berlin",
+    "France": "Paris"
 }
 print("original list (unsorted):", capitals)
 
@@ -33,5 +36,4 @@ print("original list (unsorted):", capitals)
 print("list sorted by keys:")
 
 for currentKey in sortedDictGenerator(capitals):
-	print("%s: %s" % (currentKey, capitals[currentKey]))
-
+    print("%s: %s" % (currentKey, capitals[currentKey]))
