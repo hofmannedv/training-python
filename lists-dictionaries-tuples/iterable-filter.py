1cca113e3970b269bb5c6a8bcf30f264c3143dcd
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:36:16 +0100

    use a filter on iterables, added SPDX license identifier

--- lists-dictionaries-tuples/iterable-filter.py
+++ lists-dictionaries-tuples/iterable-filter.py
@@ -1,22 +1,25 @@
 # -----------------------------------------------------------
 # demonstrates how to use the filter function
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Berlin, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a function that returns True based on a condition
 def test(operand):
-	# default return value: False
-	returnValue = False
+    # default return value: False
+    returnValue = False
 
-	# test condition
-	if (operand > 3):
-		returnValue = True
+    # test condition
+    if (operand > 3):
+        returnValue = True
 
-	# return the result of the test
-	return returnValue
+    # return the result of the test
+    return returnValue
 
 # define list of values that will be tested
 valueList = [1, 4, -5, 6]
@@ -29,5 +32,5 @@ print(valueList)
 
 # output all the values that match the condition in test()
 for element in z:
-	print(element)
+    print(element)
 
