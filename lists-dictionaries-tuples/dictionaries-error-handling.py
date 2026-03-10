dc9585fff8d51e74a9ea0d1921578a9df578e71f
Frank Hofmann <frank.hofmann@efho.de>
2026-03-10 14:32:17 +0100

    error handling for disctionaries, add SPDX license identifier, and removed has_key bug

--- lists-dictionaries-tuples/dictionaries-error-handling.py
+++ lists-dictionaries-tuples/dictionaries-error-handling.py
@@ -1,48 +1,51 @@
 # -----------------------------------------------------------
 # demonstrates different ways of error handling
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define a dictionary of capitals
 capitals = {
-	"Norway": "Oslo",
-	"Germany": "Berlin",
-	"France": "Paris"
+    "Norway": "Oslo",
+    "Germany": "Berlin",
+    "France": "Paris"
 }
 print("original:", capitals)
 
 # EAFP: Easier to Ask Forgiveness than Permission
 try:
-	city = capitals["Spain"]
-	print("The capital of Spain is %s" % city)
+    city = capitals["Spain"]
+    print("The capital of Spain is %s" % city)
 except KeyError:
-	print("There is no entry for Spain")
+    print("There is no entry for Spain")
 
 # LBYL: Look Before You Leap
 # Version 1:
 if "Spain" in capitals:
-	city = capitals["Spain"]
-	print("The capital of Spain is %s" % city)
+    city = capitals["Spain"]
+    print("The capital of Spain is %s" % city)
 else:
-	print("There is no entry for Spain")
+    print("There is no entry for Spain")
 
 # Version 2:
-if capitals.has_key("Spain"):
-	city = capitals["Spain"]
-	print("The capital of Spain is %s" % city)
+if "Spain" in capitals.keys():
+    city = capitals["Spain"]
+    print("The capital of Spain is %s" % city)
 else:
-	print("There is no entry for Spain")
+    print("There is no entry for Spain")
 
 # using the dict.get method
 # Version 1 (with default value)
 if capitals.get("Spain") == None:
-	print("There is no entry for Spain")
+    print("There is no entry for Spain")
 else:
-	city = capitals["Spain"]
-	print("The capital of Spain is %s" % city)
+    city = capitals["Spain"]
+    print("The capital of Spain is %s" % city)
 
 # Version 2 (with alternative value)
 city = capitals.get("Spain", "not in list")
