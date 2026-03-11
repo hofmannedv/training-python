ce4889004e00a56801581527721f03076c81eb8a
Frank Hofmann <frank.hofmann@efho.de>
2026-03-11 11:14:21 +0100

    extended documentation, added SPDX license identifier

--- lists-dictionaries-tuples/overview.py
+++ lists-dictionaries-tuples/overview.py
@@ -1,14 +1,18 @@
 # -----------------------------------------------------------
 # demonstrates how to create lists, dictionaries, and tuples
 #o
-# (C) 2014 Frank Hofmann, Berlin, Germany
+# (C) 2014-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
-# list of numbers
+# list of numbers, only
 numbersList = [394, 6, 15, 2, 88, 153]
-# a list with mixed values
+
+# a list with mixed values - integers, strings, floats, and boolean value
 mixedList = [1184, "orange", 3.56, True]
 print (numbersList)
 print (mixedList)
