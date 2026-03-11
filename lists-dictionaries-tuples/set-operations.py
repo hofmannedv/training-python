ce963eeaaee423f86c172b9882a37c7c3fb02877
Frank Hofmann <frank.hofmann@efho.de>
2026-03-11 11:21:49 +0100

    extended documentation, added SPDX license identifier

--- lists-dictionaries-tuples/set-operations.py
+++ lists-dictionaries-tuples/set-operations.py
@@ -1,27 +1,34 @@
 # -----------------------------------------------------------
 # demonstrates how to work with sets (basic operations)
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
-# define set of capitals
-capitals = set(["Berlin", "Oslo", "Bern"])
+# define set of capitals based on a list
+capitals = set(["Berlin", "Oslo", "Paris"])
 print("original:", capitals)
 
-# define set of other places
+# define another set of places
 places = {"Toronto", "Washington", "Berlin", "Oslo"}
 print("places:", places)
 
-# add an element
+# add an element to the second list
 capitals.add("Tokio")
 print("modified (add):", capitals)
 
 # remove an element
 # - remove() raises error if element is not present
-capitals.remove("Bern")
-print("modified (remove):", capitals)
+try:
+    capitals.remove("Bern")
+    print("modified (remove):", capitals)
+except:
+    print("cannot remove Bern from the set. Entry does not exist in the set.")
+    print("unmodified set:", capitals)
 
 # - discard() does not raise an error if element is not present
 capitals.discard("Hamburg")
