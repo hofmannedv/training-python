4db3adc29e02a4151f3e749164ac1654212d1663
Frank Hofmann <frank.hofmann@efho.de>
2026-03-11 11:00:52 +0100

    added reverse output of a list, added SPDX license identifier

--- lists-dictionaries-tuples/list-operations.py
+++ lists-dictionaries-tuples/list-operations.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates various basic list operations
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define two lists
@@ -26,9 +29,11 @@ print(citiesList)
 # - line by line using a while loop
 index = 2
 while index < 5:
-	print(citiesList[index])
-	index += 1
+    print(citiesList[index])
+    index += 1
 
 # - all at once using indexes (as a sub-list)
 print(citiesList[2:5])
 
+# output list in reverse order
+print(citiesList[::-1])
