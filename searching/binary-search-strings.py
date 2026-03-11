463449144839eb4330d8fed35e6c7ab5fb1bf803
Frank Hofmann <frank.hofmann@efho.de>
2026-03-11 15:04:45 +0100

    updated binary search for strings

---
+++ searching/binary-search-strings.py
@@ -0,0 +1,31 @@
+# -----------------------------------------------------------
+# demonstrates a binary search in a list of strings using a while loop
+#o
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+
+name = "Reto"
+sortedNamesList = ["Anna", "Berta", "Claus", "Felix", "Reto", "Urs", "Zoe"]
+
+minElement = 0
+maxElement = len(sortedNamesList)
+
+while True:
+    median = (maxElement + minElement) // 2
+    print ("min:%i max:%i median:%i" % (minElement, maxElement, median))
+    if minElement > maxElement:
+        print ("element %s not found" % name)
+        break
+    if sortedNamesList[median] == name:
+        print ("got %s at position %i" % (name, median))
+        break
+    else:
+        if sortedNamesList[median] < name:
+            minElement = median + 1
+        else:
+            maxElement = median - 1
