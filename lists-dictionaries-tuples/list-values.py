55908fc98351d01de716cf54497b6ebade2c8d4b
Frank Hofmann <frank.hofmann@efho.de>
2026-03-11 11:10:39 +0100

    extended documentation, added SPDX license identifier

--- lists-dictionaries-tuples/list-values.py
+++ lists-dictionaries-tuples/list-values.py
@@ -1,17 +1,20 @@
 # -----------------------------------------------------------
 # demonstrates how to extract the values of a dictionary using values()
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define an array (dictionary)
 capital = {
-	"France": "Paris",
-	"Switzerland": "Bern",
-	"Germany": "Berlin"
-	}
+    "France": "Paris",
+    "Switzerland": "Bern",
+    "Germany": "Berlin"
+}
 
 # output dictionary content (key-value-pairs)
 print (capital)
