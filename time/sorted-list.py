863c1a879c2b5f0e1f02a4d71852902338225fe5
Frank Hofmann <frank.hofmann@efho.de>
2026-03-25 14:53:05 +0100

    added SPDX identifier

--- time/sorted-list.py
+++ time/sorted-list.py
@@ -2,9 +2,12 @@
 # compares the creation of sorted lists using the python 
 # bisect module, and the "usual" way
 #o
-# (C) 2015 Frank Hofmann, Berlin, Germany
+# (C) 2015-2026 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # import standard modules
