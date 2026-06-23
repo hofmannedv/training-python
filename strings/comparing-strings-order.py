3aaa799d031639ba4de2915ffb7807f4942593c6
Frank Hofmann <frank.hofmann@efho.de>
2026-06-23 14:32:12 +0200

    added SPDX identifier, code simplification using f-strings

--- strings/comparing-strings-order.py
+++ strings/comparing-strings-order.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates how to compare strings (different operators)
 #o
-# (C) 2018 Frank Hofmann, Berlin, Germany
+# (C) 2018-2026 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # define the strings
@@ -11,9 +14,9 @@ listOfPlaces = ["Berlin", "Paris", "Lausanne"]
 currentCity = "Lausanne"
 
 for place in listOfPlaces:
-	if place < currentCity:
-		print ("%s comes before %s" % (place, currentCity))
-	elif place > currentCity:
-		print ("%s comes after %s" % (place, currentCity))
-	else:
-		print ("%s is similar to %s" % (place, currentCity))
+    if place < currentCity:
+        print (f"{place} comes before {currentCity}")
+    elif place > currentCity:
+        print (f"{place} comes after {currentCity}")
+    else:
+        print (f"{place} is similar to {currentCity}")
