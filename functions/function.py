98ca0fb6375482250c4387a01de378681861aa2e
Frank Hofmann <frank.hofmann@efho.de>
2026-06-23 22:04:28 +0200

    extended basic function call

--- functions/function.py
+++ functions/function.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------
 # demonstrates how to write and call a function, basically
 #o
-# (C) 2015-2025 Frank Hofmann, Berlin/Freiburg, Germany
+# (C) 2015-2026 Frank Hofmann, Berlin/Freiburg, Germany
 # email frank.hofmann@efho.de
 # 
 # License: GNU Public License (GPL) v.3.0 or later
@@ -10,18 +10,26 @@
 
 # a simple function without parameters
 def functionOne():
-	print("in functionOne.")
+    print("in functionOne.")
 
-	# define a boolean value as a return value
-	return True
+    # define a boolean value as a return value
+    return True
 
 # another function with a parameter
 def functionTwo(parameter):
-	print("in functionTwo")
-	print("parameter has this value:", parameter)
+    print("in functionTwo")
+    print("parameter has this value:", parameter)
 
-	# define a single return value
-	return 7
+    # define a single return value
+    return 7
+
+# another function with an optional parameter
+def functionThree(parameter="default value"):
+    print("in functionThree")
+    print("parameter has this value:", parameter)
+
+    # define a single return value
+    return 7
 
 # main program
 # - calling functionOne without parameters
@@ -32,4 +40,14 @@ print()
 # - calling functionTwo with a parameter 1
 returnValue2 = functionTwo(1)
 print("return value:", returnValue2)
+print()
+
+# - calling functionThree with a parameter 1
+returnValue3 = functionThree(1)
+print("return value:", returnValue3)
+print()
+
+# - calling functionThree without a parameter
+returnValue4 = functionThree()
+print("return value:", returnValue4)
 
