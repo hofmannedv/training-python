e880b382e389276ac00f7a02d7086321db877daa
Frank Hofmann <frank.hofmann@efho.de>
2026-06-23 17:47:57 +0200

    added comparing strings using a hash value, completed

--- strings/comparing-strings-hash.py
+++ strings/comparing-strings-hash.py
@@ -1,8 +1,6 @@
 # -----------------------------------------------------------
 # demonstrates how to compare strings case-sensitive
-# using a hash value
-#o
-#o#### work in progress #####################################
+# using a SHA256 hash value
 #o
 # (C) 2026 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
@@ -15,19 +13,22 @@
 # import the additional module
 import hashlib
 
-# define list of places
-listOfPlaces = [b"Bayswater", b"Table Bay", b"Bejing", b"Bombay"]
-
-# create two SHA256 hash objects
-referencePlace = b"Table Bay"
+# create a SHA256 hash object as reference
+referencePlace = "Table Bay"
 referencePlaceHashed = hashlib.sha256()
-# referencePlaceHashed.update(referencePlace.encode("utf-8"))
-referencePlaceHashed.update(referencePlace)
-placeHashed = hashlib.sha256()
+referencePlaceHashed.update(referencePlace.encode("utf-8"))
+print("Reference:")
+print(f"{referencePlace}: {referencePlaceHashed.hexdigest()}")
+print("")
 
+# define list of places
+listOfPlaces = ["Bayswater", "Table Bay", "Bejing", "Bombay"]
+
+# see if two places are identical
 for place in listOfPlaces:
-    # placeHashed.update(place.encode("utf-8"))
-    placeHashed.update(place)
-    print(placeHashed.hexdigest(), ":", referencePlaceHashed.hexdigest())
+    placeHashed = hashlib.sha256()
+    placeHashed.update(place.encode("utf-8"))
+    print(f"{place}: {placeHashed.hexdigest()}")
     if placeHashed.hexdigest() == referencePlaceHashed.hexdigest():
-        print (f"{place} matches the reference place {referencePlace}")
+        print(f"{place} and {referencePlace} are identical")
+
