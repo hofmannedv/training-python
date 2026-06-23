490ac9dea3d4d605b032597dc99f48f987f9c294
Frank Hofmann <frank.hofmann@efho.de>
2026-06-23 22:09:21 +0200

    adjusted copyright

--- calculations/calculate-square-root-multicore.py
+++ calculations/calculate-square-root-multicore.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------
 # Python Data Science examples
-# (C) 2025 Frank Hofmann, Germany 
+# (C) 2025 Frank Hofmann, Freiburg, Germany 
 # email frank.hofmann@efho.de
 # License: GNU Public License (GPL) v.3.0 or later
 # SPDX-License-Identifier: GNU General Public License v3.0 or later
