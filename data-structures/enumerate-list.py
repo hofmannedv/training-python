2787e6cee5ced002753955756d80e1c5f3b307aa
Frank Hofmann <frank.hofmann@efho.de>
2026-06-23 22:12:01 +0200

    adjusted copyright, and output using f-strings

--- data-structures/enumerate-list.py
+++ data-structures/enumerate-list.py
@@ -1,9 +1,12 @@
 # -----------------------------------------------------------
 # demonstrates how to output an enumerated list
 #o
-# (C) 2018 Frank Hofmann, Berlin, Germany
+# (C) 2018-2025 Frank Hofmann, Berlin/Freiburg, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 #
 # idea is based on: Python Tips - enumerate
 # http://book.pythontips.com/en/latest/enumerate.html
@@ -17,4 +20,4 @@ start = 1
 
 # start outputting the list of places beginning with the start number
 for number, place in enumerate(places, start):
-	print("%i: %s" % (number, place))
+    print(f"{number}: {place}")
