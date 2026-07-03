5fb10f75b082c0ec435a44b9296b78da8dfb6081
Frank Hofmann <frank.hofmann@efho.de>
2026-07-03 12:04:45 +0200

    added basic window creation using Tkinter

---
+++ gui-with-tkinter/main-window-tk.py
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------
+# demonstrates how to generate a main window using Tkinter
+#o
+# (C) 2026 Frank Hofmann, Berlin/Freiburg, Germany
+# email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+
+import tkinter as tk
+
+# define root window
+root = tk.Tk()
+
+# set the window title
+root.title("Window Example Text")
+
+# keep the window displaying
+root.mainloop()
