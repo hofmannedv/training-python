81dd7bfe69726760223966daa426b25c84dddd0f
Frank Hofmann <frank.hofmann@efho.de>
2026-07-03 11:57:38 +0200

    added Hello, World! using Tkinter

---
+++ gui-with-tkinter/hello-world-tk.py
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------
+# demonstrates how to greet you using Tkinter
+#o
+# (C) 2026 Frank Hofmann, Berlin/Freiburg, Germany
+# email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+
+import tkinter as tk
+
+# define root window
+root = tk.Tk()
+
+# add a label to the root window
+message = tk.Label(root, text="Hello, world!")
+message.pack()
+
+# keep the window displaying
+root.mainloop()
