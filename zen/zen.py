f5ea69632582c596aa5cf0fc5b1061803c01d95f
Frank Hofmann <frank.hofmann@efho.de>
2026-07-07 12:17:37 +0200

    added SPDX identifier

--- zen/zen.py
+++ zen/zen.py
@@ -1,9 +1,14 @@
 # -----------------------------------------------------------
 # show the Zen of Python
+# see https://en.wikipedia.org/wiki/Zen_of_Python for details
+# 
 #o
-# (C) 2018 Frank Hofmann, Berlin, Germany
+# (C) 2018-2026 Frank Hofmann, Freiburg/Berlin, Germany
 # Released under GNU Public License (GPL)
 # email frank.hofmann@efho.de
+#
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
 # -----------------------------------------------------------
 
 # import required this module
