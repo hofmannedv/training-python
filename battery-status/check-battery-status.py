24a5be268ac5dd8af2d3ab9cabe04ae7eb13fe19
Frank Hofmann <frank.hofmann@efho.de>
2026-07-20 01:35:34 +0200

    added battery status detection, inspiration

--- battery-status/check-battery-status.py
+++ battery-status/check-battery-status.py
@@ -6,6 +6,8 @@
 # 
 # License: GNU Public License (GPL) v.3.0 or later
 # SPDX-License-Identifier: GNU General Public License v3.0 or later
+#
+# inspired by https://x.com/clcoding/status/2078782146412466366/photo/1
 # -----------------------------------------------------------
 
 import psutil
